df716038976f051f0228a728e2d9e4276889fc7b
cnouguier <christophe.nouguier@free.fr>
2022-11-11 10:12:17 +0100

    wip: refactor client : add the functions upload/download to the client service #1

--- test/client.test.js
+++ test/client.test.js
@@ -6,11 +6,11 @@ import express from '@feathersjs/express'
 import feathersClient from '@feathersjs/client'
 import feathersSocketio from '@feathersjs/socketio'
 import io from 'socket.io-client'
-import { Service, Client } from '../lib/index.js'
+import { Service, getClientService } from '../lib/index.js'
 import fs from 'fs'
 import { Blob } from 'buffer'
 
-let serverApp, clientApp, s3Service, s3Client
+let serverApp, clientApp, s3Service, s3ClientService
 
 const options = {
   s3Client: {
@@ -42,40 +42,40 @@ describe('feathers-s3-client2', () => {
   })
   it('is ES module compatible', () => {
     expect(typeof Service).to.equal('function')
-    expect(typeof Client).to.equal('function')
+    expect(typeof getClientService).to.equal('function')
   })
   it('create the s3Service', () => {
     serverApp.use('s3', new Service(options))
     s3Service = serverApp.service('s3')
     expect(s3Service).toExist()
   })
-  it('create the s3Client', () => {
-    s3Client = new Client(clientApp, { servicePath: 's3' })
-    expect(s3Client).toExist()
+  it('create the s3ClientService', () => {
+    s3ClientService = getClientService(clientApp, { servicePath: 's3' })
+    expect(s3ClientService).toExist()
   })
   it('upload text file', async () => {
     const blob = new Blob([ fs.readFileSync('test/data/text.txt') ], { type: 'text/plain'})
-    const response = await s3Client.upload(textFileId, blob, { expiresIn: 30 })
+    const response = await s3ClientService.upload(textFileId, blob, { expiresIn: 30 })
     expect(response.ok).toExist()
     expect(response.status).to.equal(200)
     expect(response.headers.raw().etag).toExist()
   })
   it('upload image file', async () => {
     const blob = new Blob([ fs.readFileSync('test/data/image.png') ], { type: 'image/png'})
-    const response = await s3Client.upload(imageFileId, blob, 'image/png', { expiresIn: 30 })
+    const response = await s3ClientService.upload(imageFileId, blob, 'image/png', { expiresIn: 30 })
     expect(response.ok).toExist()
     expect(response.status).to.equal(200)
     expect(response.headers.raw().etag).toExist()
   })
   it('upload zip file', async () => {
     const blob = new Blob([ fs.readFileSync('test/data/archive.zip') ], { type: 'application/zip'})
-    const response = await s3Client.upload(archiveFileId, blob, 'application/zip', { expiresIn: 30 })
+    const response = await s3ClientService.upload(archiveFileId, blob, 'application/zip', { expiresIn: 30 })
     expect(response.status).to.equal(200)
     expect(response.headers.raw().etag).toExist()
   })
   it('download text file', async () => {
     const filePath = 'test/data/downloaded-text.txt'
-    const response = await s3Client.download(textFileId, 'text/plain', { expiresIn: 30 })
+    const response = await s3ClientService.download(textFileId, 'text/plain', { expiresIn: 30 })
     expect(response.ok).toExist()
     expect(response.status).to.equal(200)
     const arrayBuffer = await response.arrayBuffer()
@@ -85,7 +85,7 @@ describe('feathers-s3-client2', () => {
   })
   it('download image file', async () => {
     const filePath = 'test/data/downloaded-image.png'
-    const response = await s3Client.download(imageFileId, 'image/png', { expiresIn: 30 })
+    const response = await s3ClientService.download(imageFileId, 'image/png', { expiresIn: 30 })
     expect(response.ok).toExist()
     expect(response.status).to.equal(200)
     const arrayBuffer = await response.arrayBuffer()
@@ -95,7 +95,7 @@ describe('feathers-s3-client2', () => {
   })
   it('download zip file', async () => {
     const filePath = 'test/data/downloaded-archive.zip'
-    const response = await s3Client.download(archiveFileId, 'application/zip', { expiresIn: 30 })
+    const response = await s3ClientService.download(archiveFileId, 'application/zip', { expiresIn: 30 })
     expect(response.ok).toExist()
     expect(response.status).to.equal(200)
     const arrayBuffer = await response.arrayBuffer()
@@ -104,17 +104,17 @@ describe('feathers-s3-client2', () => {
     fs.unlinkSync(filePath)
   })
   it('delete text file', async () => {
-    const response = await s3Client.remove(textFileId, { expiresIn: 30 })
+    const response = await s3ClientService.remove(textFileId, { expiresIn: 30 })
     expect(response.ok).toExist()
     expect(response.status).to.equal(200)
   })
   it('delete image file', async () => {
-    const response = await s3Client.remove(imageFileId, { expiresIn: 30 })
+    const response = await s3ClientService.remove(imageFileId, { expiresIn: 30 })
     expect(response.ok).toExist()
     expect(response.status).to.equal(200)
   })
   it('delete archive file', async () => {
-    const response = await s3Client.remove(archiveFileId, { expiresIn: 30 })
+    const response = await s3ClientService.remove(archiveFileId, { expiresIn: 30 })
     expect(response.ok).toExist()
     expect(response.status).to.equal(200)
   })
