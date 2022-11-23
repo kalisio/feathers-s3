3262a065e1af56f58dddf5b4c39593ae06825f36
cnouguier <christophe.nouguier@free.fr>
2022-11-23 12:25:32 +0100

    test: adapt prefix for each test suite

--- test/service.test.js
+++ test/service.test.js
@@ -23,7 +23,7 @@ const options = {
     signatureVersion: 'v4'
   },
   bucket: process.env.S3_BUCKET,
-  prefix: 'feathers-s3-service'
+  prefix: 'feathers-s3-test-service'
 }
 
 const fileId = 'features.geojson'
